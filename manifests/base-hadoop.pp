95499a04c70c613de9b2641027a95e2afaa98f6a
Cristian Pereyra <cristian@redmintlabs.com>
2013-09-22 11:56:34 -0300

    sets up the hadoop cluster

---
+++ manifests/base-hadoop.pp
@@ -0,0 +1,43 @@
+include hadoop
+group { "puppet":
+  ensure => "present",
+}
+
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+}
+
+package { "openjdk-6-jdk":
+   ensure => present,
+  require => Exec['apt-get update']
+}
+
+exec{ "create_ssh_folder": 
+  command => '/bin/mkdir -p /root/.ssh',
+  unless => '/usr/bin/test -d /root/.ssh',
+  require => Exec['apt-get update']
+}
+
+
+file {
+  "/root/.ssh/id_rsa":
+  source => "puppet:///modules/hadoop/id_rsa",
+  mode => 600,
+  owner => root,
+  group => root,
+  require => Exec['create_ssh_folder']
+ }
+ 
+file {
+  "/root/.ssh/id_rsa.pub":
+  source => "puppet:///modules/hadoop/id_rsa.pub",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec['create_ssh_folder']
+ }
+
+exec { "add_authorized_key": 
+  command => '/bin/cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys',
+  require => File['/root/.ssh/id_rsa.pub']
+}
---
+++ modules/hadoop/manifests/init.pp
@@ -0,0 +1,71 @@
+class hadoop {
+ $hadoop_home = "/opt/hadoop"
+
+exec { "download_hadoop":
+command => "wget -O /tmp/hadoop.tar.gz http://apache.xfree.com.ar/hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz",
+path => $path,
+unless => "ls /opt | grep hadoop-1.2.1",
+require => Package["openjdk-6-jdk"]
+}
+
+exec { "unpack_hadoop":
+  command => "tar -zxf /tmp/hadoop.tar.gz -C /opt",
+  path => $path,
+  creates => "${hadoop_home}-1.2.1",
+  require => Exec["download_hadoop"]
+}
+
+file {
+  "${hadoop_home}-1.2.1/conf/slaves":
+  source => "puppet:///modules/hadoop/slaves",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+ 
+file {
+  "${hadoop_home}-1.2.1/conf/masters":
+  source => "puppet:///modules/hadoop/masters",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+
+file {
+  "${hadoop_home}-1.2.1/conf/core-site.xml":
+  source => "puppet:///modules/hadoop/core-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+ 
+file {
+  "${hadoop_home}-1.2.1/conf/mapred-site.xml":
+  source => "puppet:///modules/hadoop/mapred-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+ 
+ file {
+  "${hadoop_home}-1.2.1/conf/hdfs-site.xml":
+  source => "puppet:///modules/hadoop/hdfs-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+ 
+  file {
+  "${hadoop_home}-1.2.1/conf/hadoop-env.sh":
+  source => "puppet:///modules/hadoop/hadoop-env.sh",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["unpack_hadoop"]
+ }
+}
